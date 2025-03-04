e5bce65109d7d585cacd45de0d7cf035900d9774
sponge254 <g.stuk@outlook.com>
2025-03-04 17:44:00 -0500

    Fixed bugs in loading script

--- src/scripts/generateTeams.ts
+++ src/scripts/generateTeams.ts
@@ -20,16 +20,44 @@ const options = {
 fetch(url, options)
     .then(response => response.json())
     .then(json => {
-        const matches: Record<number, {red: {1: number, 2: number, 3: number}, blue: {1: number, 2: number, 3: number}}> = {};
+        const qmatches: Record<number, { key: string, red: { 1: number, 2: number, 3: number }, blue: { 1: number, 2: number, 3: number } }> = {};
+        var smatches: Record<number, { key: string,  red: { 1: number, 2: number, 3: number }, blue: { 1: number, 2: number, 3: number } }> = {};
+        var fmatches: Record<number, { key: string, red: { 1: number, 2: number, 3: number }, blue: { 1: number, 2: number, 3: number } }> = {};
         for (var i = 0; i < json.length; i++) {
             const alliances = json[i]["alliances"];
-            matches[i] = {
-                red: { 1: parseInt(alliances["red"]["team_keys"][0].replace("frc", "")), 2: parseInt(alliances["red"]["team_keys"][1].replace("frc", "")), 3: parseInt(alliances["red"]["team_keys"][2].replace("frc", "")) },
-                blue: { 1: parseInt(alliances["blue"]["team_keys"][0].replace("frc", "")), 2: parseInt(alliances["blue"]["team_keys"][1].replace("frc", "")), 3: parseInt(alliances["blue"]["team_keys"][2].replace("frc", "")) }
+            console.log(`Current Match: ${json[i]["key"]}`)
+            if (json[i]["comp_level"] === 'f') {
+                console.log(`   - Sorted as: final${Object.keys(fmatches).length + 1}`)
+                fmatches[parseInt(json[i]["match_number"])] = {
+                    key: json[i]["key"],
+                    red: { 1: parseInt(alliances["red"]["team_keys"][0].replace("frc", "")), 2: parseInt(alliances["red"]["team_keys"][1].replace("frc", "")), 3: parseInt(alliances["red"]["team_keys"][2].replace("frc", "")) },
+                    blue: { 1: parseInt(alliances["blue"]["team_keys"][0].replace("frc", "")), 2: parseInt(alliances["blue"]["team_keys"][1].replace("frc", "")), 3: parseInt(alliances["blue"]["team_keys"][2].replace("frc", "")) }
+                }
+            } else if (json[i]["comp_level"] === 'sf') {
+                console.log(`   - Sorted as: sf${Object.keys(smatches).length + 1}`)
+                smatches[parseInt(json[i]["set_number"])] = {
+                    key: json[i]["key"],
+                    red: { 1: parseInt(alliances["red"]["team_keys"][0].replace("frc", "")), 2: parseInt(alliances["red"]["team_keys"][1].replace("frc", "")), 3: parseInt(alliances["red"]["team_keys"][2].replace("frc", "")) },
+                    blue: { 1: parseInt(alliances["blue"]["team_keys"][0].replace("frc", "")), 2: parseInt(alliances["blue"]["team_keys"][1].replace("frc", "")), 3: parseInt(alliances["blue"]["team_keys"][2].replace("frc", "")) }
+                }
+            } else {
+                console.log(`   - Sorted as: qm${Object.keys(qmatches).length + 1}`)
+                qmatches[parseInt(json[i]["match_number"])] = {
+                    key: json[i]["key"],
+                    red: { 1: parseInt(alliances["red"]["team_keys"][0].replace("frc", "")), 2: parseInt(alliances["red"]["team_keys"][1].replace("frc", "")), 3: parseInt(alliances["red"]["team_keys"][2].replace("frc", "")) },
+                    blue: { 1: parseInt(alliances["blue"]["team_keys"][0].replace("frc", "")), 2: parseInt(alliances["blue"]["team_keys"][1].replace("frc", "")), 3: parseInt(alliances["blue"]["team_keys"][2].replace("frc", "")) }
+                }
             }
         }
 
-        const jsonData = JSON.stringify({event_id: eventId, matches}, null, 4);
+        qmatches
+
+        console.log(`Total: ${Object.keys(qmatches).length} quals, ${Object.keys(smatches).length} elims, ${Object.keys(fmatches).length} finals`)
+        const n = Object.keys(qmatches).length;
+        const n2 = Object.keys(smatches).length;
+        smatches = Object.fromEntries(Object.entries(smatches).map(([key, value]) => [String(Number(key) + n), value]));
+        fmatches = Object.fromEntries(Object.entries(fmatches).map(([key, value]) => [String(Number(key) + n + n2), value]))
+        const jsonData = JSON.stringify({ event_id: eventId, matches: { ...qmatches, ...smatches, ...fmatches} }, null, 4);
         const outputPath = path.join(process.cwd(), 'public/teams.json');
         fs.writeFileSync(outputPath, jsonData, 'utf-8');
         console.log(`✅ Read matches from ${eventId} (${url}) into: ${outputPath}`);
