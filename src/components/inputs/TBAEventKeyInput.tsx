a93375d87b135ffe9775f67e51c254f1122a54d6
sponge254 <g.stuk@outlook.com>
2026-04-02 22:45:23 -0400

    add non-load event backup, made alliance color constant

--- src/components/inputs/TBAEventKeyInput.tsx
+++ src/components/inputs/TBAEventKeyInput.tsx
@@ -3,7 +3,8 @@ import { ConfigurableInputProps } from "./ConfigurableInput";
 import { TBAEventKeyInputData } from "./BaseInputProps";
 import { Textarea } from "../ui/textarea";
 import { useEvent } from "@/hooks";
-import { useMemo } from "react";
+import React, { useEffect, useMemo } from "react";
+import { Input } from "../ui/input";
 
 export default function TBAEventKeyInput(props: ConfigurableInputProps) {
     const data = useQRScoutState(
@@ -12,26 +13,43 @@ export default function TBAEventKeyInput(props: ConfigurableInputProps) {
 
     const matchData = useQRScoutState(state => state.matchData);
 
-    if (!data) {
-        return <div>Invalid input</div>;
+    const [value, setValue] = React.useState<string>(!matchData ? "" : matchData[0].event_key);
+
+    useMemo(() => updateValue(props.code, value), []);
+
+    useEffect(() => {
+        if (data && matchData && matchData.length > 0) {
+            setValue(matchData[0].event_key);
+        }
+    }, [matchData])
+
+    useEvent('resetFields', () => setValue(!matchData ? value : matchData[0].event_key));
+
+    useEffect(() => {
+        updateValue(props.code, value);
+    }, [value, props.code]);
+
+    if (!data || !matchData || matchData?.length < 0) {
+        return (
+    <Input
+      type="text"
+      value={value || ''}
+      id={data?.title}
+      onChange={e => {
+        console.log(e.target.value);
+        setValue(e.target.value);
+      }}
+      placeholder="Enter event key"
+    />
+  );
     }
-
-    if (!matchData || matchData?.length < 0) {
-        return <div>No match data</div>;
-    }
-
-    const key = matchData[0].event_key;
-
-    useMemo(() => updateValue(props.code, key), []);
-
-    useEvent('resetFields', () => updateValue(props.code, key));
 
     return (
         <Textarea
             disabled={true}
             name={`${data.title}_display`}
             id={`${data.title}_display`}
-            value={key}
+            value={value}
         >key</Textarea>
     )
 }
